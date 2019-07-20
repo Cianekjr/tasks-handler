1dd6bb65ef56bc6eacc94377676d001da0c658f1
Mateusz Chrzanowski <Cianekjr@gmail.com>
2019-07-20 16:50:30 +0200

    feat(Theme): Add theme resuable styles

--- src/Themes/colors.js
+++ src/Themes/colors.js
@@ -2,7 +2,8 @@ export const text = {
   primary: '#FFF',
   secondary: '#000',
   gallery: '#EEE',
-  footer: 'rgba(255, 255, 255, .3)'
+  footer: 'rgba(255, 255, 255, .3)',
+  tagName: '#EEE'
 };
 
 export const bgColors = {
@@ -10,5 +11,6 @@ export const bgColors = {
   main: '#313131',
   footer: '#141414',
   card: '#404040',
-  task: '#555'
+  task: '#555',
+  scrollbar: '#555',
 };
--- src/Themes/commons.js
+++ src/Themes/commons.js
@@ -1,20 +1,26 @@
 export const boxShadows = {
   primary: '0 0px 10px #5bbc5f',
   secondary: '0 0px 10px #141414',
+  button: 'inset 0 -6px 0 -3px rgba(0,0,0,0.17)'
 };
 
 export const borders = {
   primary: '1px solid #888',
   cardTitle: '1px solid #EEE',
-  task: '3px solid #353535'
+  task: '3px solid #353535',
+  tag: '2px solid #EEE'
 };
 
 export const borderRadius = {
+  button: '2',
   card: '10',
+  scrollbar: '10'
 };
 
 export const fontSizes = {
-  cardTitle: '18'
+  cardTitle: '1.4',
+  taskTitle: '1',
+  tagName: '0.8',
 };
 
 export const style = {
