ced926ddc0b53cfc35e143fcb719f3ae6fe71527
Mateusz Chrzanowski <Cianekjr@gmail.com>
2019-07-20 16:48:41 +0200

    feat(Tasks): Add initial tasks

---
+++ src/Utils/json/DBTasks.js
@@ -0,0 +1,46 @@
+const tasksInitial = [
+  {
+    id: 2,
+    section: 'backlog',
+    title: 'Fix query GET_ID',
+    description: '',
+    tags: ['frontend', 'flyhacks']
+  },
+  {
+    id: 3,
+    section: 'in_progress',
+    title: 'Fix logo in header',
+    description: 'Use svg instead png.',
+    tags: ['backend', 'nethone']
+  },
+  {
+    id: 4,
+    section: 'planned',
+    title: 'Add footer',
+    description: '',
+    tags: ['backend', 'flyhacks']
+  },
+  {
+    id: 5,
+    section: 'code_review',
+    title: 'Fix footer description',
+    description: '',
+    tags: ['frontend', 'nethone', 'important']
+  },
+  {
+    id: 6,
+    section: 'planned',
+    title: 'Add component primary-passenger',
+    description: 'Calendar should be created by using library. Tooltips are separate task.',
+    tags: []
+  },
+  {
+    id: 7,
+    section: 'planned',
+    title: 'Add component header',
+    description: '',
+    tags: ['frontend']
+  }
+];
+
+export default tasksInitial;
